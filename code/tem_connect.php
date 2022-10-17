335d80d3f29a374a2f0fe9d4857c6efa5a518f37
Rus <punyarit07@gmail.com>
2022-10-18 02:36:49 +0700

    template

---
+++ code/tem_connect.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+    $conn = new mysqli("localhost", "root", "", "problemcomplaintsystem");
+    if ($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+?>
