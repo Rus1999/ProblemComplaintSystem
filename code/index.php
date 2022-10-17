e0f84879054436dfc1177a19f0922f31134b7447
Rus <punyarit07@gmail.com>
2022-10-18 02:37:45 +0700

    index.php

---
+++ code/index.php
@@ -0,0 +1,18 @@
+<?php
+    include("./tem_connect.php");
+    include("./tem_header.php");
+    include("./tem_menu.php");
+?>
+
+
+
+
+
+
+
+
+<?php
+    // close connection
+    $conn->close();
+    include("./tem_footer.php");
+?>
